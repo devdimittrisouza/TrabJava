38d1cf7cbd9947c08adbc4ad4f0b9c7345dc1d63
Rafael <rafadograu11@hotmail.com>
2023-11-22 22:35:34 -0300

    Adoção classe Motoboy

---
+++ TrabalhoJava/src/Classe/MotoBoyDto.java
@@ -0,0 +1,51 @@
+package Classe;
+
+public class MotoBoyDto {
+
+	private String nome;
+	private String login;
+	private String senha;
+
+	// Criando o construtor da classe
+	public MotoBoyDto(String nome, String login, String senha) {
+
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	// Getters e Setters
+
+	public String getNome() {
+
+		return nome;
+	}
+
+	public String getLogin() {
+
+		return login;
+	}
+
+	public String getSenha() {
+
+		return senha;
+	}
+
+	// Setters
+	public void setNome(String nome) {
+
+		this.nome = nome;
+	}
+
+	public void setLogin(String login) {
+
+		this.login = login;
+	}
+
+	public void setSenha(String senha) {
+
+		this.senha = senha;
+	}
+}
+
+ 

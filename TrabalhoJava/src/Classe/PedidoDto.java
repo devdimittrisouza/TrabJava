863d951103b265bd5e131c12b5ab6e7c884deae8
Rafael <rafadograu11@hotmail.com>
2023-11-23 22:43:40 -0300

    Adilçao do padrão Prototype na classe PedidoDto

--- TrabalhoJava/src/Classe/PedidoDto.java
+++ TrabalhoJava/src/Classe/PedidoDto.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class PedidoDto {
 
 	private int numPedido;
-	private Date dataCompra;
+	private String dataCompra;
 	private Double valorFrete;
 	private String nomeCliente;
 	private int numCliente;
@@ -13,7 +13,7 @@ public class PedidoDto {
 	private String formaPagamento;
 	private int valor;
 	
-	public PedidoDto(int numPedido, Date dataCompra, Double valorFrete, String nomeCliente, int numCliente, String status, String formaPagamento, int valor) {
+	public PedidoDto(int numPedido, String dataCompra, Double valorFrete, String nomeCliente, int numCliente, String status, String formaPagamento, int valor) {
 		// TODO Auto-generated constructor stub
 		this.numPedido = numPedido;
 		this.dataCompra = dataCompra;
@@ -31,7 +31,7 @@ public class PedidoDto {
 		return numPedido;
 	}
 	
-	public Date getDataCompra() {
+	public String getDataCompra() {
 		
 		return dataCompra;
 	}
@@ -65,4 +65,33 @@ public class PedidoDto {
 		
 		return valor;
 	}
+	
+	// Método clone para aplicar o padrão Prototype
+    public PedidoDto pedirNovamente() {
+        return new PedidoDto(
+                this.numPedido,
+                this.dataCompra,
+                this.valorFrete,
+                this.nomeCliente,
+                this.numCliente,
+                this.status,
+                this.formaPagamento,
+                this.valor
+        );
+    }
+
+    // Exemplo de uso
+    public static void main(String[] args) {
+    	PedidoDto pedidoOriginal = new PedidoDto (1, "2023-11-23", 10.9, "Cliente Teste", 123, "Em andamento", "Cartão de Crédito", 100);
+    	PedidoDto pedidoClonado = pedidoOriginal.pedirNovamente();
+
+        // Exibindo informações do pedido clonado
+        System.out.println("Informações do Pedido Original:");
+        System.out.println("Número do Pedido: " + pedidoOriginal.getNumPedido());
+        System.out.println("Cliente: " + pedidoOriginal.getNomeCliente());
+
+        System.out.println("\nInformações do Pedido Clonado:");
+        System.out.println("Número do Pedido: " + pedidoClonado.getNumPedido());
+        System.out.println("Cliente: " + pedidoClonado.getNomeCliente());
+    }
 }

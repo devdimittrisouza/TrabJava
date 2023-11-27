c412b1ff98901852befcaaaff526c4e071759033
Dimittri Souza <dimittrisouza@gmail.com>
2023-11-26 21:29:20 -0300

    Versão final v1

--- TrabalhoJava/src/Classe/PedidoDto.java
+++ TrabalhoJava/src/Classe/PedidoDto.java
@@ -2,18 +2,23 @@ package Classe;
 
 import java.util.Date;
 
-public class PedidoDto {
+public class PedidoDto implements Cloneable {
 
 	private int numPedido;
-	private String dataCompra;
+	private Date dataCompra;
 	private Double valorFrete;
 	private String nomeCliente;
 	private int numCliente;
 	private String status;
 	private String formaPagamento;
 	private int valor;
-	
-	public PedidoDto(int numPedido, String dataCompra, Double valorFrete, String nomeCliente, int numCliente, String status, String formaPagamento, int valor) {
+
+	public PedidoDto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PedidoDto(int numPedido, Date dataCompra, Double valorFrete, String nomeCliente, int numCliente,
+			String status, String formaPagamento, int valor) {
 		// TODO Auto-generated constructor stub
 		this.numPedido = numPedido;
 		this.dataCompra = dataCompra;
@@ -24,88 +29,144 @@ public class PedidoDto {
 		this.formaPagamento = formaPagamento;
 		this.valor = valor;
 	}
-	
-	//Getter
+
+	public PedidoDto clonar() {
+		// Utilize o construtor ou método de criação com parâmetros para criar uma cópia
+		return new PedidoDto(this.numPedido, this.dataCompra, this.valorFrete, this.nomeCliente, this.numCliente,
+				this.status, this.formaPagamento, this.valor);
+	}
+
+	// Getter
 	public int getNumPedido() {
-		
+
 		return numPedido;
 	}
-	
-	public String getDataCompra() {
-		
+
+	public Date getDataCompra() {
+
 		return dataCompra;
 	}
-	
+
 	public Double getValorFrete() {
-		
+
 		return valorFrete;
 	}
-	
+
 	public String getNomeCliente() {
-		
+
 		return nomeCliente;
 	}
-	
+
 	public int getNumCliente() {
-		
+
 		return numCliente;
 	}
-	
+
 	public String getStatus() {
-		
+
 		return status;
 	}
-	
+
 	public String getFormaPagamento() {
-		
+
 		return formaPagamento;
 	}
-	
+
 	public int getValor() {
-		
+
 		return valor;
 	}
-	
-	// Método clone para aplicar o padrão Prototype
-    public PedidoDto pedirNovamente() {
-        return new PedidoDto(
-                this.numPedido,
-                this.dataCompra,
-                this.valorFrete,
-                this.nomeCliente,
-                this.numCliente,
-                this.status,
-                this.formaPagamento,
-                this.valor
-        );
-    }
 
-    // Exemplo de uso
-    public static void main(String[] args) {
-    	PedidoDto pedidoOriginal = new PedidoDto (1, "2023-11-23", 10.9, "Cliente Teste", 123, "Em andamento", "Cartão de Crédito", 100);
-    	PedidoDto pedidoClonado = pedidoOriginal.pedirNovamente();
+	public void setNumPedido(int numPedido) {
+		this.numPedido = numPedido;
+	}
 
-        // Exibindo informações do pedido clonado
-        System.out.println("Informações do Pedido Original:");
-        System.out.println("Número do Pedido: " + pedidoOriginal.getNumPedido());
-        System.out.println("Data da compra:  " + pedidoOriginal.getDataCompra());
-        System.out.println("Valor do frete: " + pedidoOriginal.getValorFrete());
-        System.out.println("Cliente: " + pedidoOriginal.getNomeCliente());
-        System.out.println("Numero do Cliente: " + pedidoOriginal.getNumCliente());
-        System.out.println("Status: " + pedidoOriginal.getStatus());
-        System.out.println("Forma de Pagamento: " + pedidoOriginal.getFormaPagamento());
-        System.out.println("Valor: " + pedidoOriginal.getValor());
+	public void setDataCompra(Date dataCompra) {
+		this.dataCompra = dataCompra;
+	}
 
-        System.out.println("\nInformações do Pedido Clonado:");
-        System.out.println("Número do Pedido: " + pedidoClonado.getNumPedido());
-        System.out.println("Data da compra:  " + pedidoClonado.getDataCompra());
-        System.out.println("Valor do frete: " + pedidoClonado.getValorFrete());
-        System.out.println("Cliente: " + pedidoClonado.getNomeCliente());
-        System.out.println("Numero do Cliente: " + pedidoClonado.getNumCliente());
-        System.out.println("Status: " + pedidoClonado.getStatus());
-        System.out.println("Forma de Pagamento: " + pedidoClonado.getFormaPagamento());
-        System.out.println("Valor: " + pedidoClonado.getValor());
-        
-    }
-} 
- 
+	public void setValorFrete(Double valorFrete) {
+		this.valorFrete = valorFrete;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public void setNumCliente(int numCliente) {
+		this.numCliente = numCliente;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	/*
+	 * // Método clone para aplicar o padrão Prototype public PedidoDto
+	 * pedirNovamente() { return new PedidoDto( this.numPedido, this.dataCompra,
+	 * this.valorFrete, this.nomeCliente, this.numCliente, this.status,
+	 * this.formaPagamento, this.valor ); }
+	 */
+
+	public PedidoDto pedirNovamente() {
+		try {
+			return (PedidoDto) super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// Exemplo de uso
+	public static void main(String[] args) {
+		PedidoDto pedidoOriginal = new PedidoDto(1, new Date(), 10.0, "Cliente1", 123, "Em andamento",
+				"Cartão de Crédito", 100);
+		PedidoDto pedidoClonado = pedidoOriginal.pedirNovamente();
+
+		/*
+		 * // Exibindo informações do pedido clonado
+		 * System.out.println("Informações do Pedido Original:");
+		 * System.out.println("Número do Pedido: " + pedidoOriginal.getNumPedido());
+		 * System.out.println("Data da compra:  " + pedidoOriginal.getDataCompra());
+		 * System.out.println("Valor do frete: " + pedidoOriginal.getValorFrete());
+		 * System.out.println("Cliente: " + pedidoOriginal.getNomeCliente());
+		 * System.out.println("Numero do Cliente: " + pedidoOriginal.getNumCliente());
+		 * System.out.println("Status: " + pedidoOriginal.getStatus());
+		 * System.out.println("Forma de Pagamento: " +
+		 * pedidoOriginal.getFormaPagamento()); System.out.println("Valor: " +
+		 * pedidoOriginal.getValor());
+		 * 
+		 * System.out.println("\nInformações do Pedido Clonado:");
+		 * System.out.println("Número do Pedido: " + pedidoClonado.getNumPedido());
+		 * System.out.println("Data da compra:  " + pedidoClonado.getDataCompra());
+		 * System.out.println("Valor do frete: " + pedidoClonado.getValorFrete());
+		 * System.out.println("Cliente: " + pedidoClonado.getNomeCliente());
+		 * System.out.println("Numero do Cliente: " + pedidoClonado.getNumCliente());
+		 * System.out.println("Status: " + pedidoClonado.getStatus());
+		 * System.out.println("Forma de Pagamento: " +
+		 * pedidoClonado.getFormaPagamento()); System.out.println("Valor: " +
+		 * pedidoClonado.getValor());
+		 */
+
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	public String toString() {
+		return "Número do Pedido: " + numPedido + "\nData da compra: " + dataCompra + "\nValor do frete: " + valorFrete
+				+ "\nCliente: " + nomeCliente + "\nNumero do Cliente: " + numCliente + "\nStatus: " + status
+				+ "\nForma de Pagamento: " + formaPagamento + "\nValor: " + valor;
+	}
+
+}
--- TrabalhoJava/src/Logica/App.java
+++ TrabalhoJava/src/Logica/App.java
@@ -1,8 +1,13 @@
 package Logica;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+
+import Classe.PedidoDto;
 
 public class App {
 
@@ -12,6 +17,11 @@ public class App {
 	private static boolean loggedIn = false;
 	private static List<String> carrinho = new ArrayList<>();
 	private static List<String> historicoPedidos = new ArrayList<>();
+	private static String[] sabores = { "Calabresa", "Frango com Catupiry", "Marguerita" };
+	private static String[] tamanhos = { "Pequena", "Média", "Grande" };
+	
+	private static Map<Integer, PedidoDto> mapaHistorico = new HashMap<>();
+    private static int numeroPedidoAtual = 1;
 
 	public static void main(String[] args) {
 		exibirMenuInicial();
@@ -133,7 +143,8 @@ public class App {
 			System.out.println("1- Portuguesa");
 			System.out.println("2- Peperoni");
 			System.out.println("3- Costela");
-			System.out.println("4- Voltar");
+			System.out.println("4- Pizza Personalizada");
+			System.out.println("5- Voltar");
 
 			System.out.print("Escolha uma pizza: ");
 			opcao = scanner.nextInt();
@@ -149,13 +160,49 @@ public class App {
 				exibirDetalhesPizza("Costela", 50.0);
 				break;
 			case 4:
+				criarPizzaPersonalizada();
+				break;
+			case 5:
 				System.out.println("Voltando ao menu principal.");
 				break;
 			default:
 				System.out.println("Opção inválida. Tente novamente.");
 			}
 
-		} while (opcao != 4);
+		} while (opcao != 5);
+	}
+
+	private static void criarPizzaPersonalizada() {
+		PizzaPersonalizadaBuilder builder = new App().new PizzaPersonalizadaBuilder();
+		PizzaDiretor diretor = new PizzaDiretor();
+
+		System.out.println("Monte sua pizza personalizada!");
+
+		System.out.println("Escolha o sabor da pizza:");
+		System.out.println("1. Calabresa");
+		System.out.println("2. Frango com Catupiry");
+		System.out.println("3. Marguerita");
+		int escolhaSabor = scanner.nextInt();
+		scanner.nextLine(); // Limpa o buffer
+
+		System.out.println("Escolha o tamanho da pizza:");
+		System.out.println("1. Pequena");
+		System.out.println("2. Média");
+		System.out.println("3. Grande");
+		int escolhaTamanho = scanner.nextInt();
+		scanner.nextLine(); // Limpa o buffer
+
+		diretor.construct(builder, sabores[escolhaSabor - 1], tamanhos[escolhaTamanho - 1]);
+
+		Pizza pizzaPersonalizada = builder.getPizza();
+		exibirDetalhesPizza("Personalizada", calcularValorPersonalizada(pizzaPersonalizada));
+	}
+
+	private static double calcularValorPersonalizada(Pizza pizza) {
+		// Lógica para calcular o valor de uma pizza personalizada
+		// ... (pode variar dependendo dos ingredientes e outros fatores)
+		return 60.0; // Valor de exemplo
+
 	}
 
 	private static void adicionarAoCarrinho(String nomeItem, double valorItem) {
@@ -293,11 +340,41 @@ public class App {
 
 			switch (opcao) {
 			case 1:
-				pagamentoCartaoCredito();
+				PedidoDto pedidoCartaoCredito = new PedidoDto();
+                // Configurar as informações do pedido
+                pedidoCartaoCredito.setNumPedido(numeroPedidoAtual);
+                pedidoCartaoCredito.setDataCompra(new Date());
+                pedidoCartaoCredito.setValorFrete(0.0);
+                pedidoCartaoCredito.setNomeCliente(usuarioPadrao);
+                pedidoCartaoCredito.setNumCliente(1);
+                pedidoCartaoCredito.setStatus("Em andamento");
+                pedidoCartaoCredito.setFormaPagamento("Cartão de Crédito");
+                pedidoCartaoCredito.setValor(100);
+
+                historicoPedidos.add(pedidoCartaoCredito.toString());
+                mapaHistorico.put(numeroPedidoAtual, pedidoCartaoCredito);
+                numeroPedidoAtual++;
+                carrinho.clear();
+
+                System.out.println("Pagamento realizado com sucesso! Aguarde a entrega.");
+                
 				break;
 			case 2:
+				PedidoDto pedidoCartaoDebito = new PedidoDto();
+				
+				pedidoCartaoDebito.setNumPedido(numeroPedidoAtual);
+				pedidoCartaoDebito.setDataCompra(new Date());
+				pedidoCartaoDebito.setValorFrete(0.0);
+				pedidoCartaoDebito.setNomeCliente(usuarioPadrao);
+				pedidoCartaoDebito.setNumCliente(1);
+				pedidoCartaoDebito.setStatus("Em andamento");
+				pedidoCartaoDebito.setFormaPagamento("Cartão de Débito");
+				pedidoCartaoDebito.setValor(100);
+				
 				System.out.println("Pedido realizado! Aguarde a entrega.");
-				historicoPedidos.add(carrinho.toString());
+				historicoPedidos.add(pedidoCartaoDebito.toString());
+				mapaHistorico.put(numeroPedidoAtual, pedidoCartaoDebito);
+                numeroPedidoAtual++;
 				carrinho.clear();
 				break;
 			case 3:
@@ -346,9 +423,89 @@ public class App {
 		if (historicoPedidos.isEmpty()) {
 			System.out.println("O histórico de pedidos está vazio.");
 		} else {
-			for (String pedido : historicoPedidos) {
-				System.out.println(pedido);
+			for (int i = 0; i < historicoPedidos.size(); i++) {
+                String pedido = historicoPedidos.get(i);
+                System.out.println((i + 1) + "- " + pedido);
+            }
+			
+			System.out.print("Escolha o número do pedido que deseja repetir (0 para voltar): ");
+	        int escolha = scanner.nextInt();
+
+	        if (escolha > 0 && escolha <= historicoPedidos.size()) {
+	            PedidoDto pedidoOriginal = mapaHistorico.get(escolha);
+	            PedidoDto pedidoClonado = pedidoOriginal.pedirNovamente();
+
+	            // Exibindo informações do pedido clonado
+	            System.out.println("Informações do Pedido Clonado:");
+	            System.out.println("Número do Pedido: " + pedidoClonado.getNumPedido());
+	            System.out.println("Data da compra:  " + pedidoClonado.getDataCompra());
+	            System.out.println("Valor do frete: " + pedidoClonado.getValorFrete());
+	            System.out.println("Cliente: " + pedidoClonado.getNomeCliente());
+	            System.out.println("Numero do Cliente: " + pedidoClonado.getNumCliente());
+	            System.out.println("Status: " + pedidoClonado.getStatus());
+	            System.out.println("Forma de Pagamento: " + pedidoClonado.getFormaPagamento());
+	            System.out.println("Valor: " + pedidoClonado.getValor());
+
+	            // Adicionando o pedido clonado ao carrinho
+	            carrinho.add(pedidoClonado.toString());
+	        } else if (escolha != 0) {
+	            System.out.println("Opção inválida. Tente novamente.");
+	        }
+		}
+	}
+
+	class PizzaPersonalizadaBuilder implements PizzaBuilder {
+		private Pizza pizza;
+
+		PizzaPersonalizadaBuilder() {
+			this.reset();
+		}
+
+		@Override
+		public void reset() {
+			this.pizza = new Pizza();
+		}
+
+		@Override
+		public void buildSabor(String sabor) {
+			pizza.setSabor(sabor);
+		}
+
+		@Override
+		public void buildTamanho(String tamanho) {
+			pizza.setTamanho(tamanho);
+		}
+
+		@Override
+		public void buildBorda() {
+			System.out.println("Escolha o tipo de borda para a pizza:");
+			System.out.println("1. Queijo");
+			System.out.println("2. Catupiry");
+			System.out.println("3. Cheddar");
+
+			Scanner scanner = new Scanner(System.in);
+			int escolhaBorda = scanner.nextInt();
+			scanner.nextLine(); // Limpa o buffer
+
+			switch (escolhaBorda) {
+			case 1:
+				pizza.setBorda("queijo");
+				break;
+			case 2:
+				pizza.setBorda("catupiry");
+				break;
+			case 3:
+				pizza.setBorda("cheddar");
+				break;
+			default:
+				System.out.println("Opção inválida. Usando borda padrão (queijo).");
+				pizza.setBorda("queijo");
 			}
 		}
+
+		Pizza getPizza() {
+			return pizza;
+		}
+
 	}
 }
